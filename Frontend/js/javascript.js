3ed2dd11500eb838a26c192c190d711fe4a3a23b
Evgexajs <ekopytov509@gmail.com>
2021-11-05 19:42:18 +0500

    test

---
+++ Frontend/js/javascript.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: {
+          delay: 3000,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+}
+function navigation() {
+    if (document.querySelector('.navigation').classList.contains('active')) {
+        document.querySelector('.navigation').classList.remove('active')
+        document.querySelector('.navigation__burger').classList.remove('active')
+    } else {
+        document.querySelector('.navigation').classList.add('active')
+        document.querySelector('.navigation__burger').classList.add('active')
+    }
+}
